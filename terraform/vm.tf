eb3a78f097e40269763c2f2a5d2da680220d3f5c
Muhammad Bintang Bahy <bahybintang@gmail.com>
2023-04-24 06:41:10 +0700

    init

---
+++ terraform/vm.tf
@@ -0,0 +1,41 @@
+resource "azurerm_virtual_machine" "master" {
+  name                             = "master-vm"
+  location                         = azurerm_resource_group.cloud.location
+  resource_group_name              = azurerm_resource_group.cloud.name
+  network_interface_ids            = [azurerm_network_interface.internal.id]
+  vm_size                          = "Standard_DS1_v2"
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "master-vm-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "master"
+    admin_username = "k3s"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/k3s/.ssh/authorized_keys"
+      key_data = azurerm_ssh_public_key.main.public_key
+    }
+  }
+
+  tags = {
+    env          = "dev"
+    orchestrator = "k3s"
+  }
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,44 @@
+resource "azurerm_virtual_network" "cloud" {
+  name                = "cloud-network"
+  location            = azurerm_resource_group.cloud.location
+  resource_group_name = azurerm_resource_group.cloud.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.cloud.name
+  virtual_network_name = azurerm_virtual_network.cloud.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "external" {
+  name                 = "external"
+  resource_group_name  = azurerm_resource_group.cloud.name
+  virtual_network_name = azurerm_virtual_network.cloud.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.cloud.name
+  virtual_network_name = azurerm_virtual_network.cloud.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                = "dev-nic"
+  location            = azurerm_resource_group.cloud.location
+  resource_group_name = azurerm_resource_group.cloud.name
+
+  ip_configuration {
+    name                          = "dev-nic-config"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
