1761ac5ea6ded2420c687b6f0a0f56a6dfd7540e
Muhammad Bintang Bahy <bahybintang@gmail.com>
2023-05-08 22:49:55 +0700

    remove bastion to save cost :')

--- terraform/k3s/security-group.tf
+++ terraform/k3s/security-group.tf
@@ -1,3 +1,4 @@
+# Very dumb and insecure security group for k3s. This is just for testing purposes.
 resource "azurerm_network_security_group" "k3s" {
   name                = "k3s"
   location            = azurerm_resource_group.cloud.location
@@ -10,7 +11,7 @@ resource "azurerm_network_security_group" "k3s" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_ranges    = ["2379", "2380", "6443", "8472", "10250", "51820", "51821", "51871"]
+    destination_port_ranges    = ["22", "2379", "2380", "6443", "8472", "10250", "51820", "51821", "51871"]
     source_address_prefixes    = ["10.0", "10.112", "103.82.14.237", azurerm_public_ip.nat.ip_address]
     destination_address_prefix = "*"
   }
